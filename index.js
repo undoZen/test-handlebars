9f6fdc785ac6cea418db97ecd03184a61af99ae0
Anson Yu Wang <opensource@undozen.com>
2014-11-30 12:14:39 +0800

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+var Handlebars = require('handlebars')
+var source = "<p>Hello{{a.b.c}}, my name is {{name}}. I am from {{hometown}}. I have " +
+             "{{kids.length}} kids:</p>" +
+             "<ul>{{#kids}}<li>{{name}} is {{age}}</li>{{/kids}}</ul>";
+var template = Handlebars.compile(source);
+
+var data = { "name": "Alan", "hometown": "Somewhere, TX",
+             "kids": [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}]};
+var result = template(data);
+console.log(result);
+
+// Would render:
+// <p>Hello, my name is Alan. I am from Somewhere, TX. I have 2 kids:</p>
+// <ul>
+//   <li>Jimmy is 12</li>
+//   <li>Sally is 4</li>
+// </ul>
